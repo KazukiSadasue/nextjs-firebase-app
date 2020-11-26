e8c01de07f2ac75844ad726c8c814380c044797b
KazukiSadasue <kazuki.fx0915@gmail.com>
2020-11-26 09:49:37 +0900

    vercelの環境変数4KB超え問題を解決

--- lib/firebase_admin.ts
+++ lib/firebase_admin.ts
@@ -1,13 +1,19 @@
 import * as admin from 'firebase-admin'
 
-// とりあえず
 if (admin.apps.length == 0) {
-  const credential = JSON.parse(
-    Buffer.from(
-      process.env.GCP_CREDENTIAL.replace(/\\n/g, '\n'),
-      'base64'
-    ).toString()
-  )
+  // 多分コメントアウトしている情報は不要なので削除(vercelの環境変数に4KB超えを設定出来ないため)
+  const credential = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    // type: process.env.FIREBASE_ACCOUNT_TYPE,
+    // private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    // client_id: process.env.FIREBASE_CLIENT_ID,
+    // auth_uri: process.env.FIREBASE_AUTH_URI,
+    // token_uri: process.env.FIREBASE_TOKEN_URI,
+    // auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    // client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }
 
   admin.initializeApp({
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
